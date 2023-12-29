22efed9201bfb32835e2891411216ce0304a2a02
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-12-29 23:54:56 +0100

    Init tanstack router demo

---
+++ examples/demo-tanstack-router/README.md
@@ -0,0 +1,4 @@
+# Oidc-spa Tanstack Router Example
+
+This example project demonstrates how to use the oidc-spa library with Tanstack Router for building a Single Page Application (SPA) with OpenID Connect (OIDC) authentication.
+
---
+++ examples/demo-tanstack-router/src/router/Layout.tsx
@@ -0,0 +1,23 @@
+import reactLogo from 'assets/react.svg'
+import viteLogo from 'assets/vite.svg'
+import './App.css'
+import { Outlet } from '@tanstack/react-router'
+import { AuthStatus } from '../AuthStatus'
+
+export function Layout() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Outlet />
+      <AuthStatus />
+    </>
+  )
+}
+
