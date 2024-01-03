5575a21f4ead2dd36ce3df77aae7b9d7e6b32598
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-01-03 10:33:32 +0100

    breakpoint

--- examples/tanstack-router/src/pages/ProtectedPage.tsx
+++ examples/tanstack-router/src/pages/ProtectedPage.tsx
@@ -1,3 +1,14 @@
+import { useOidc } from "oidc";
+
 export function ProtectedPage() {
-    return <h1>Protected Page</h1>;
+    // Here we can safely assume that the user is logged in.
+    const { oidcTokens } = useOidc({ assertUserLoggedIn: true });
+
+    return (
+        <h4>
+            Hello {oidcTokens.decodedIdToken.preferred_username}
+            <br />
+            The page you are currently viewing can only be accessed when you are authenticated.
+        </h4>
+    );
 }
