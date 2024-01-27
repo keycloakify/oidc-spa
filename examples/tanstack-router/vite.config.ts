10f59657a80ce3055dbe578e6c545438a8946bfb
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-01-27 05:02:08 +0100

    Dynamique linking of CJS modules in Vite :party:

--- examples/tanstack-router/vite.config.ts
+++ examples/tanstack-router/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import commonjs from "vite-plugin-commonjs";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    resolve: {
-        preserveSymlinks: true
-    }
+    plugins: [react(), tsconfigPaths(), commonjs()]
 });
