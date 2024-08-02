504349dd2c09d96cfc2c1374055854748455db71
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-08-02 18:41:02 +0200

    fmt

--- examples/tanstack-router-file-based/src/routeTree.gen.ts
+++ examples/tanstack-router-file-based/src/routeTree.gen.ts
@@ -10,46 +10,46 @@
 
 // Import Routes
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as ProtectedImport } from "./routes/protected";
-import { Route as IndexImport } from "./routes/index";
+import { Route as rootRoute } from './routes/__root'
+import { Route as ProtectedImport } from './routes/protected'
+import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
 
 const ProtectedRoute = ProtectedImport.update({
-    path: "/protected",
-    getParentRoute: () => rootRoute
-} as any);
+  path: '/protected',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
-    path: "/",
-    getParentRoute: () => rootRoute
-} as any);
+  path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module "@tanstack/react-router" {
-    interface FileRoutesByPath {
-        "/": {
-            id: "/";
-            path: "/";
-            fullPath: "/";
-            preLoaderRoute: typeof IndexImport;
-            parentRoute: typeof rootRoute;
-        };
-        "/protected": {
-            id: "/protected";
-            path: "/protected";
-            fullPath: "/protected";
-            preLoaderRoute: typeof ProtectedImport;
-            parentRoute: typeof rootRoute;
-        };
+declare module '@tanstack/react-router' {
+  interface FileRoutesByPath {
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexImport
+      parentRoute: typeof rootRoute
     }
+    '/protected': {
+      id: '/protected'
+      path: '/protected'
+      fullPath: '/protected'
+      preLoaderRoute: typeof ProtectedImport
+      parentRoute: typeof rootRoute
+    }
+  }
 }
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren({ IndexRoute, ProtectedRoute });
+export const routeTree = rootRoute.addChildren({ IndexRoute, ProtectedRoute })
 
 /* prettier-ignore-end */
 
