7e8da2d38e195de18de19c1f1ff55c15b7cb7e8f
Joseph Garrone <joseph.garrone.gj@gmail.com>
2025-01-24 03:41:38 +0100

    checkpoint

--- src/oidc/createOidc.ts
+++ src/oidc/createOidc.ts
@@ -20,7 +20,6 @@ import { toHumanReadableDuration } from "../tools/toHumanReadableDuration";
 import { createHybridStorage } from "../tools/HybridStorage";
 import { toFullyQualifiedUrl } from "../tools/toFullyQualifiedUrl";
 import { OidcInitializationError } from "./OidcInitializationError";
-import { getIFrameTimeoutDelayMs } from "./iframeTimeoutDelay";
 import { getStateData, type StateData } from "./StateData";
 import { notifyOtherTabOfLogout, getPrOtherTabLogout } from "./logoutPropagationToOtherTabs";
 import { getConfigHash } from "./configHash";
@@ -707,224 +706,161 @@ export async function createOidc_nonMemoized<
         restore_from_http_only_cookie: {
             log?.("Trying to restore the auth from the httpOnly cookie (silent signin with iframe)");
 
-            type AuthResponse = {
-                state: string;
-                [key: string]: string;
-            };
-
-            const dAuthResponse = new Deferred<AuthResponse | undefined>();
-
-            const timeoutDelayMs = getIFrameTimeoutDelayMs();
-
-            const timeout = setTimeout(async () => {
-                let dedicatedSilentSsoHtmlFileCsp: string | null | undefined = undefined;
-
-                oidc_callback_htm_unreachable: {
-                    if (!urls.hasDedicatedHtmFile) {
-                        break oidc_callback_htm_unreachable;
-                    }
-
-                    const getHtmFileReachabilityStatus = async (ext?: "html") =>
-                        fetch(`${urls.callbackUrl}${ext === "html" ? "l" : ""}`).then(
-                            async response => {
-                                dedicatedSilentSsoHtmlFileCsp =
-                                    response.headers.get("Content-Security-Policy");
-
-                                const content = await response.text();
-
-                                return content.length < 1200 &&
-                                    content.includes("parent.postMessage(authResponse")
-                                    ? "ok"
-                                    : "reachable but wrong content";
-                            },
-                            () => "not reachable" as const
-                        );
-
-                    const status = await getHtmFileReachabilityStatus();
-
-                    if (status === "ok") {
-                        break oidc_callback_htm_unreachable;
-                    }
-
-                    dAuthResponse.reject(
-                        new OidcInitializationError({
-                            "type": "bad configuration",
-                            "likelyCause": {
-                                "type": "oidc-callback.htm not properly served",
-                                "oidcCallbackHtmUrl": urls.callbackUrl,
-                                "likelyCause": await (async () => {
-                                    if ((await getHtmFileReachabilityStatus("html")) === "ok") {
-                                        return "using .html instead of .htm extension";
-                                    }
-
-                                    switch (status) {
-                                        case "not reachable":
-                                            return "the file hasn't been created";
-                                        case "reachable but wrong content":
-                                            return "serving another file";
-                                    }
-                                })()
-                            }
-                        })
-                    );
-                    return;
+            const result_loginSilent = await loginOrLogoutSilent({
+                oidcClientTsUserManager,
+                configHash,
+                "action": {
+                    "type": "login",
+                    getExtraTokenParams
                 }
+            });
 
-                frame_ancestors_none: {
-                    const csp = await (async () => {
-                        if (urls.hasDedicatedHtmFile) {
-                            assert(dedicatedSilentSsoHtmlFileCsp !== undefined);
-                            return dedicatedSilentSsoHtmlFileCsp;
-                        }
-
-                        const csp = await fetch(urls.callbackUrl).then(
-                            response => response.headers.get("Content-Security-Policy"),
-                            error => id<Error>(error)
-                        );
-
-                        if (csp instanceof Error) {
-                            dAuthResponse.reject(
-                                new Error(`Failed to fetch ${urls.callbackUrl}: ${csp.message}`)
-                            );
-                            return new Promise<never>(() => {});
-                        }
-
-                        return csp;
-                    })();
-
-                    if (csp === null) {
-                        break frame_ancestors_none;
-                    }
-
-                    const hasFrameAncestorsNone = csp
-                        .replace(/["']/g, "")
-                        .replace(/\s+/g, " ")
-                        .toLowerCase()
-                        .includes("frame-ancestors none");
-
-                    if (!hasFrameAncestorsNone) {
-                        break frame_ancestors_none;
-                    }
-
-                    dAuthResponse.reject(
-                        new OidcInitializationError({
-                            "type": "bad configuration",
-                            "likelyCause": {
-                                "type": "frame-ancestors none",
-                                urls
-                            }
-                        })
-                    );
-                    return;
-                }
-
-                // Here we know that the server is not down and that the issuer_uri is correct
-                // otherwise we would have had a fetch error when loading the iframe.
-                // So this means that it's very likely a OIDC client misconfiguration.
-                // It could also be a very slow network but this risk is mitigated by the fact that we check
-                // for the network speed to adjust the timeout delay.
-                dAuthResponse.reject(
-                    new OidcInitializationError({
-                        "type": "bad configuration",
-                        "likelyCause": {
-                            "type": "misconfigured OIDC client",
-                            clientId,
-                            timeoutDelayMs,
-                            "callbackUrl": urls.callbackUrl
-                        }
-                    })
-                );
-            }, timeoutDelayMs);
-
-            const listener = (event: MessageEvent) => {
-                function getIsAuthResponse(data: any): data is AuthResponse {
-                    return data instanceof Object && "state" in data && typeof data.state === "string";
-                }
-
-                if (!getIsAuthResponse(event.data)) {
-                    return;
-                }
-
-                const authResponse = event.data;
-
-                const stateData = getStateData({ state: authResponse.state });
-
-                if (stateData === undefined) {
-                    return;
-                }
-
-                if (stateData.configHash !== configHash) {
-                    return;
-                }
-
-                clearTimeout(timeout);
-
-                window.removeEventListener("message", listener);
-
-                {
-                    const error: string | undefined = authResponse["error"];
-
-                    if (error !== undefined) {
-                        log?.(`The auth server responded with: ${error}`);
-                        dAuthResponse.resolve(undefined);
-                        return;
-                    }
-                }
-
-                dAuthResponse.resolve(authResponse);
-            };
-
-            window.addEventListener("message", listener, false);
-
-            oidcClientTsUserManager
-                .signinSilent({
-                    "silentRequestTimeoutInSeconds": timeoutDelayMs / 1000,
-                    "extraTokenParams": getExtraTokenParams?.(),
-                    "state": id<StateData>({
-                        configHash,
-                        "isSilentSso": true
-                    })
-                })
-                .catch((error: Error) => {
-                    if (error.message === "Failed to fetch") {
-                        clearTimeout(timeout);
-
-                        // Here we know it's not web origin because it's not the token we are fetching
-                        // but just the well known configuration endpoint that is not subject to CORS.
-                        dAuthResponse.reject(
-                            new OidcInitializationError({
+            if (!result_loginSilent.isSuccess) {
+                const initializationError: OidcInitializationError = await (async () => {
+                    switch (result_loginSilent.cause) {
+                        case "server down":
+                            return new OidcInitializationError({
                                 "type": "server down",
                                 issuerUri
-                            })
-                        );
+                            });
+                        case "timeout": {
+                            let dedicatedSilentSsoHtmlFileCsp: string | null | undefined = undefined;
+
+                            oidc_callback_htm_unreachable: {
+                                if (!urls.hasDedicatedHtmFile) {
+                                    break oidc_callback_htm_unreachable;
+                                }
+
+                                const getHtmFileReachabilityStatus = async (ext?: "html") =>
+                                    fetch(`${urls.callbackUrl}${ext === "html" ? "l" : ""}`).then(
+                                        async response => {
+                                            dedicatedSilentSsoHtmlFileCsp =
+                                                response.headers.get("Content-Security-Policy");
+
+                                            const content = await response.text();
+
+                                            return content.length < 1200 &&
+                                                content.includes("parent.postMessage(authResponse")
+                                                ? "ok"
+                                                : "reachable but wrong content";
+                                        },
+                                        () => "not reachable" as const
+                                    );
+
+                                const status = await getHtmFileReachabilityStatus();
+
+                                if (status === "ok") {
+                                    break oidc_callback_htm_unreachable;
+                                }
+
+                                return new OidcInitializationError({
+                                    "type": "bad configuration",
+                                    "likelyCause": {
+                                        "type": "oidc-callback.htm not properly served",
+                                        "oidcCallbackHtmUrl": urls.callbackUrl,
+                                        "likelyCause": await (async () => {
+                                            if ((await getHtmFileReachabilityStatus("html")) === "ok") {
+                                                return "using .html instead of .htm extension";
+                                            }
+
+                                            switch (status) {
+                                                case "not reachable":
+                                                    return "the file hasn't been created";
+                                                case "reachable but wrong content":
+                                                    return "serving another file";
+                                            }
+                                        })()
+                                    }
+                                });
+                            }
+
+                            frame_ancestors_none: {
+                                const csp = await (async () => {
+                                    if (urls.hasDedicatedHtmFile) {
+                                        assert(dedicatedSilentSsoHtmlFileCsp !== undefined);
+                                        return dedicatedSilentSsoHtmlFileCsp;
+                                    }
+
+                                    const csp = await fetch(urls.callbackUrl).then(
+                                        response => response.headers.get("Content-Security-Policy"),
+                                        error => id<Error>(error)
+                                    );
+
+                                    if (csp instanceof Error) {
+                                        return new Error(
+                                            `Failed to fetch ${urls.callbackUrl}: ${csp.message}`
+                                        );
+                                    }
+
+                                    return csp;
+                                })();
+
+                                if (csp instanceof Error) {
+                                    return csp;
+                                }
+
+                                if (csp === null) {
+                                    break frame_ancestors_none;
+                                }
+
+                                const hasFrameAncestorsNone = csp
+                                    .replace(/["']/g, "")
+                                    .replace(/\s+/g, " ")
+                                    .toLowerCase()
+                                    .includes("frame-ancestors none");
+
+                                if (!hasFrameAncestorsNone) {
+                                    break frame_ancestors_none;
+                                }
+
+                                return new OidcInitializationError({
+                                    "type": "bad configuration",
+                                    "likelyCause": {
+                                        "type": "frame-ancestors none",
+                                        urls
+                                    }
+                                });
+                            }
+
+                            // Here we know that the server is not down and that the issuer_uri is correct
+                            // otherwise we would have had a fetch error when loading the iframe.
+                            // So this means that it's very likely a OIDC client misconfiguration.
+                            // It could also be a very slow network but this risk is mitigated by the fact that we check
+                            // for the network speed to adjust the timeout delay.
+                            return new OidcInitializationError({
+                                "type": "bad configuration",
+                                "likelyCause": {
+                                    "type": "misconfigured OIDC client",
+                                    clientId,
+                                    timeoutDelayMs,
+                                    "callbackUrl": urls.callbackUrl
+                                }
+                            });
+                        }
                     }
-                });
+                })();
 
-            const authResponse = await dAuthResponse.pr;
-
-            if (authResponse === undefined) {
-                log?.("There is no active session");
-                break restore_from_http_only_cookie;
+                throw initializationError;
             }
 
-            const { authResponseUrl } = (() => {
-                let authResponseUrl = "https://dummy.com";
+            const { authResponse } = result_loginSilent;
 
-                for (const [name, value] of Object.entries(authResponse)) {
-                    authResponseUrl = addQueryParamToUrl({
-                        "url": authResponseUrl,
-                        name,
-                        value
-                    }).newUrl;
+            {
+                const error: string | undefined = authResponse["error"];
+
+                if (error !== undefined) {
+                    // NOTE: This is a very expected case, it happens each time there's no active session.
+                    log?.(`The auth server responded with: ${error}`);
+                    break restore_from_http_only_cookie;
                 }
-
-                return { authResponseUrl };
-            })();
+            }
 
             let oidcClientTsUser: OidcClientTsUser | undefined = undefined;
 
             try {
-                oidcClientTsUser = await oidcClientTsUserManager.signinRedirectCallback(authResponseUrl);
+                oidcClientTsUser = await oidcClientTsUserManager.signinRedirectCallback(
+                    authResponseToUrl(authResponse)
+                );
             } catch (error) {
                 assert(error instanceof Error);
 
