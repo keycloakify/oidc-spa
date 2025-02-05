c31685b7c825cdc219ded3bc90492cca6bc057ca
garronej <Joseph.garrone@protonmail.com>
2025-02-05 14:33:12 +0100

    Restrict the state lenght to 8 characters

--- src/oidc/configHash.ts
+++ src/oidc/configHash.ts
@@ -1,13 +1,15 @@
 import { fnv1aHash } from "../tools/fnv1aHash";
 
-const SIGNATURE = "aa8689";
+const SIGNATURE = "af7";
 
 export function getConfigHash(params: { issuerUri: string; clientId: string }) {
-    return `${fnv1aHash(`${params.issuerUri} ${params.clientId}`)}${SIGNATURE}`;
+    return `${fnv1aHash(`${params.issuerUri} ${params.clientId}`).slice(0, -3)}${SIGNATURE}`;
 }
+
+const CONFIG_HASH_REGEXP = new RegExp(`^[0-9a-f]{5}${SIGNATURE}$`);
 
 export function getIsConfigHash(params: { maybeConfigHash: string }): boolean {
     const { maybeConfigHash } = params;
 
-    return maybeConfigHash.endsWith(SIGNATURE);
+    return CONFIG_HASH_REGEXP.test(maybeConfigHash);
 }
