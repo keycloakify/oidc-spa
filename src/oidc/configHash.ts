095dd20687bd0e3ff8a2bc4041ba917982e9ff14
garronej <joseph.garrone@protonmail.com>
2025-02-08 10:59:17 +0100

    Remove any chances to missidentify the state param

--- src/oidc/configHash.ts
+++ src/oidc/configHash.ts
@@ -1,12 +1,12 @@
 import { fnv1aHash } from "../tools/fnv1aHash";
 
-const SIGNATURE = "af7";
+const SIGNATURE = "af794b1e3a";
 
 export function getConfigHash(params: { issuerUri: string; clientId: string }) {
-    return `${fnv1aHash(`${params.issuerUri} ${params.clientId}`).slice(0, -3)}${SIGNATURE}`;
+    return `${fnv1aHash(`${params.issuerUri} ${params.clientId}`)}${SIGNATURE}`;
 }
 
-const CONFIG_HASH_REGEXP = new RegExp(`^[0-9a-f]{5}${SIGNATURE}$`);
+const CONFIG_HASH_REGEXP = new RegExp(`^[0-9a-f]{8}${SIGNATURE}$`);
 
 export function getIsConfigHash(params: { maybeConfigHash: string }): boolean {
     const { maybeConfigHash } = params;
