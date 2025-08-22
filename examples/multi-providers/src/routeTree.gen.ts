413b4734ab370b577e4f18a94b147da7e49bb8d3
garronej <joseph.garrone@protonmail.com>
2025-08-22 17:24:23 +0200

    Update every example setup

--- examples/multi-providers/src/routeTree.gen.ts
+++ examples/multi-providers/src/routeTree.gen.ts
@@ -8,65 +8,34 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-// Import Routes
+import { Route as rootRouteImport } from "./routes/__root";
+import { Route as ProtectedRouteImport } from "./routes/protected";
+import { Route as IndexRouteImport } from "./routes/index";
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as ProtectedImport } from "./routes/protected";
-import { Route as IndexImport } from "./routes/index";
-
-// Create/Update Routes
-
-const ProtectedRoute = ProtectedImport.update({
+const ProtectedRoute = ProtectedRouteImport.update({
     id: "/protected",
     path: "/protected",
-    getParentRoute: () => rootRoute
+    getParentRoute: () => rootRouteImport
 } as any);
-
-const IndexRoute = IndexImport.update({
+const IndexRoute = IndexRouteImport.update({
     id: "/",
     path: "/",
-    getParentRoute: () => rootRoute
+    getParentRoute: () => rootRouteImport
 } as any);
-
-// Populate the FileRoutesByPath interface
-
-declare module "@tanstack/react-router" {
-    interface FileRoutesByPath {
-        "/": {
-            id: "/";
-            path: "/";
-            fullPath: "/";
-            preLoaderRoute: typeof IndexImport;
-            parentRoute: typeof rootRoute;
-        };
-        "/protected": {
-            id: "/protected";
-            path: "/protected";
-            fullPath: "/protected";
-            preLoaderRoute: typeof ProtectedImport;
-            parentRoute: typeof rootRoute;
-        };
-    }
-}
-
-// Create and export the route tree
 
 export interface FileRoutesByFullPath {
     "/": typeof IndexRoute;
     "/protected": typeof ProtectedRoute;
 }
-
 export interface FileRoutesByTo {
     "/": typeof IndexRoute;
     "/protected": typeof ProtectedRoute;
 }
-
 export interface FileRoutesById {
-    __root__: typeof rootRoute;
+    __root__: typeof rootRouteImport;
     "/": typeof IndexRoute;
     "/protected": typeof ProtectedRoute;
 }
-
 export interface FileRouteTypes {
     fileRoutesByFullPath: FileRoutesByFullPath;
     fullPaths: "/" | "/protected";
@@ -75,35 +44,34 @@ export interface FileRouteTypes {
     id: "__root__" | "/" | "/protected";
     fileRoutesById: FileRoutesById;
 }
-
 export interface RootRouteChildren {
     IndexRoute: typeof IndexRoute;
     ProtectedRoute: typeof ProtectedRoute;
+}
+
+declare module "@tanstack/react-router" {
+    interface FileRoutesByPath {
+        "/protected": {
+            id: "/protected";
+            path: "/protected";
+            fullPath: "/protected";
+            preLoaderRoute: typeof ProtectedRouteImport;
+            parentRoute: typeof rootRouteImport;
+        };
+        "/": {
+            id: "/";
+            path: "/";
+            fullPath: "/";
+            preLoaderRoute: typeof IndexRouteImport;
+            parentRoute: typeof rootRouteImport;
+        };
+    }
 }
 
 const rootRouteChildren: RootRouteChildren = {
     IndexRoute: IndexRoute,
     ProtectedRoute: ProtectedRoute
 };
-
-export const routeTree = rootRoute._addFileChildren(rootRouteChildren)._addFileTypes<FileRouteTypes>();
-
-/* ROUTE_MANIFEST_START
-{
-  "routes": {
-    "__root__": {
-      "filePath": "__root.tsx",
-      "children": [
-        "/",
-        "/protected"
-      ]
-    },
-    "/": {
-      "filePath": "index.tsx"
-    },
-    "/protected": {
-      "filePath": "protected.tsx"
-    }
-  }
-}
-ROUTE_MANIFEST_END */
+export const routeTree = rootRouteImport
+    ._addFileChildren(rootRouteChildren)
+    ._addFileTypes<FileRouteTypes>();
