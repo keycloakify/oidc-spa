78487eb6c48be24f677b363b36bb46698b845961
garronej <joseph.garrone@protonmail.com>
2025-08-22 12:19:09 +0200

    Update react-router framework example

--- examples/react-router-framework/app/entry.client.lazy.tsx
+++ examples/react-router-framework/app/entry.client.lazy.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
-import { OidcProvider } from "./oidc.client";
+import { getOidc, OidcProvider } from "./oidc.client";
 
-ReactDOM.hydrateRoot(
-    document,
-    <React.StrictMode>
-        {/* 
-        NOTE: Do not provide a fallback prop to the <OidcProvider>
-        Use the HydrateFallback of ./root.tsx instead
-        */}
-        <OidcProvider>
-            <HydratedRouter />
-        </OidcProvider>
-    </React.StrictMode>
-);
+getOidc().then(() => {
+    ReactDOM.hydrateRoot(
+        document,
+        <React.StrictMode>
+            {/*
+             * NOTE: Do not provide a fallback prop to the <OidcProvider>
+             * Use the HydrateFallback of ./root.tsx instead
+             */}
+            <OidcProvider>
+                <HydratedRouter />
+            </OidcProvider>
+        </React.StrictMode>
+    );
+});
