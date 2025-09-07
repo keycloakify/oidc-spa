abb7fd106ca0f1fbe30b4207ab248be27f1e2faa
garronej <joseph.garrone@protonmail.com>
2025-09-07 02:34:27 +0200

    No need for commonjs vite plugin anymore

--- examples/react-router-framework/vite.config.ts
+++ examples/react-router-framework/vite.config.ts
@@ -1,12 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-// NOTE: This plugin is for devolvement purposes (to dynamically link the local version of oidc-spa)
-// You don't need this in your project.
-import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
-    plugins: [reactRouter(), tsconfigPaths(), process.env["IS_LINKED"] === "true" && commonjs()],
+    plugins: [reactRouter(), tsconfigPaths()],
     optimizeDeps: {
         include: [
             "oidc-spa/react",
