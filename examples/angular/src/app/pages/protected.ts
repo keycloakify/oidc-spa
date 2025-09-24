d5eea8a1b16e1784b2a92bf3c5c49bf466c5f234
garronej <joseph.garrone@protonmail.com>
2025-09-24 23:45:13 +0200

    Remove unused imports

--- examples/angular/src/app/pages/protected.ts
+++ examples/angular/src/app/pages/protected.ts
@@ -1,5 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
-import { createKeycloakUtils, isKeycloak } from 'oidc-spa/keycloak';
+import { Component } from '@angular/core';
 import { getOidc, get$decodedIdToken } from '../../oidc';
 
 @Component({
