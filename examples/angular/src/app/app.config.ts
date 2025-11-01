25028162c79fbae76dc76487e05749aec0e901a6
garronej <joseph.garrone@protonmail.com>
2025-11-01 16:38:56 +0100

    Remove unused inject import and clean up app configuration

--- examples/angular/src/app/app.config.ts
+++ examples/angular/src/app/app.config.ts
@@ -2,7 +2,6 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
-  inject,
 } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
