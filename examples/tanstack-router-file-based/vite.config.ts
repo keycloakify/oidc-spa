5b0b7d56effc23dde3c1930748a5b1c477e3e094
garronej <joseph.garrone@protonmail.com>
2025-11-03 03:58:37 +0100

    Remove legacy examples

--- examples/tanstack-router-file-based/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-vite-plugin";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [tanstackRouter(), react()]
-});
