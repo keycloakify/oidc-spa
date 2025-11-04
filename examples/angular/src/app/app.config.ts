b624fee04a8f5871a745b1835c3cdab7e51c71f7
garronej <joseph.garrone@protonmail.com>
2025-11-04 05:11:58 +0100

    Recommend context token for angular

--- examples/angular/src/app/app.config.ts
+++ examples/angular/src/app/app.config.ts
@@ -16,6 +16,8 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([
         Oidc.createBearerInterceptor({
           shouldInjectAccessToken: (req) =>
+            // NOTE: This is the legacy and familiar pattern but we do not recommend it.
+            // Prefer using per request context token: https://github.com/keycloakify/oidc-spa/blob/c0f13614c59d38e111bf0af6f6b36c71b74018f1/examples/angular-kitchensink/src/app/app.config.ts#L30-L42
             /^(https:\/\/jsonplaceholder\.typicode\.com)(\/.*)?$/i.test(req.url),
         }),
       ])
