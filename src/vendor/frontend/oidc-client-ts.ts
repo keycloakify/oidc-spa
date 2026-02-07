a545a3ad711ecb013fd450058b829147e0c473d0
garronej <joseph.garrone@protonmail.com>
2026-02-07 19:41:25 +0100

    Improve handling of errors when refreshing tokens

--- src/vendor/frontend/oidc-client-ts.ts
+++ src/vendor/frontend/oidc-client-ts.ts
@@ -1,2 +1,8 @@
-export { UserManager, WebStorageStateStore, InMemoryWebStorage } from "oidc-client-ts";
+export {
+    UserManager,
+    WebStorageStateStore,
+    InMemoryWebStorage,
+    ErrorTimeout,
+    ErrorResponse
+} from "oidc-client-ts";
 export type { User, OidcMetadata } from "oidc-client-ts";
