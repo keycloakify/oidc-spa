d0de40925088bac94a93480d8bd46f461bab714e
garronej <joseph.garrone@protonmail.com>
2026-05-26 01:27:29 +0200

    Export createUser from core index

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,4 @@ export type { Oidc } from "./Oidc";
 export { createOidc, type ParamsOfCreateOidc } from "./createOidc";
 export { OidcInitializationError } from "./OidcInitializationError";
 export { oidcEarlyInit } from "./earlyInit";
+export type CreateUser<User> = import("./createOidc").ParamsOfCreateOidc.CreateUser<User>;
