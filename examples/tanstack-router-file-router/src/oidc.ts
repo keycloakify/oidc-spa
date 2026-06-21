808b8b912f9e8603196f7ba6f25403dd268f4dec
garronej <joseph.garrone@protonmail.com>
2026-06-21 16:36:39 +0200

    Fix TanStack Router example

--- examples/tanstack-router-file-router/src/oidc.ts
+++ examples/tanstack-router-file-router/src/oidc.ts
@@ -28,3 +28,26 @@ bootstrapOidc(
               debugLogs: false
           }
 );
+
+/**
+ * A convenience wrapper around `fetch()` that automatically
+ * attaches the access token as an Authorization header when the user is logged in.
+ *
+ * Usage:
+ *   const response = await fetchWithAuth("/api/data");
+ *
+ * If you need to talk to more than one resource server read this:
+ * https://docs.oidc-spa.dev/v/v10/talking-to-multiple-apis-with-different-access-tokens
+ */
+export const fetchWithAuth: typeof fetch = async (input, init) => {
+    const oidc = await getOidc();
+
+    if (oidc.isUserLoggedIn) {
+        const accessToken = await oidc.getAccessToken();
+        const headers = new Headers(init?.headers);
+        headers.set("Authorization", `Bearer ${accessToken}`);
+        (init ??= {}).headers = headers;
+    }
+
+    return fetch(input, init);
+};
